7974075dc6ee282a69b17ecd7f6117639b9cf73a
Chyisiang <chyisiang8@gmail.com>
2023-05-28 11:59:17 +0800

    react router dome implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react"
-import "./style.css"
+import { useEffect, useState } from "react";
+import "./style.css";
 import { TodoForm } from "./components/TodoForm";
 import { TodoList } from "./components/TodoList";
 import { Calculator } from "./components/Calculator";
 import { Navbar } from "./components/Navbar";
 import { Home } from "./components/Home";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   const [todos, setTodos] = useState(() => {
@@ -16,64 +17,58 @@ export default function App() {
   const [newItem, setNewItem] = useState("");
 
   useEffect(() => {
-    localStorage.setItem("ITEMS", JSON.stringify(todos))
-  }, [todos])
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(title, description) {
-    setTodos(currentTodos => {
+    setTodos((currentTodos) => {
       return [
         ...currentTodos,
         { id: crypto.randomUUID(), title, description, completed: false },
-      ]
-    })
+      ];
+    });
     setNewItem("");
   }
 
   function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
         if (todo.id === id) {
           return { ...todo, completed };
         }
 
         return todo;
-      })
-    })
+      });
+    });
   }
 
   function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-  }
-
-  let component
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home/>
-      break
-    case "/calculator":
-      component = <Calculator />
-      break
-    case "/TodoForm":
-      component = (
-      <>
-        <TodoForm onSubmit={addTodo} />
-        <h1 className="header">Todo List</h1>
-        <TodoList
-          todos={todos}
-          toggleTodo={toggleTodo}
-          deleteTodo={deleteTodo}
-        />
-      </>
-    );
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   return (
     <>
       <Navbar />
-      {component}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route
+          path="/todoform"
+          element={
+            <>
+              <TodoForm onSubmit={addTodo} />
+              <h1 className="header">Todo List</h1>
+              <TodoList
+                todos={todos}
+                toggleTodo={toggleTodo}
+                deleteTodo={deleteTodo}
+              />
+            </>
+          }
+        />
+      </Routes>
     </>
-
-  )
+  );
 }
