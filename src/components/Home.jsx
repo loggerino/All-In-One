b7771b74fe931de93a444ebc586cf039118eca39
Chyisiang <chyisiang8@gmail.com>
2023-06-01 17:45:28 +0800

    stupid vercel case sensitive rule

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,15 @@ export function Home() {
       <h1 className="home-title">Apps</h1>
       <ul className="homelist">
         <li>
-          <img src="/public/calculator.jpg" alt="Calculator" className="app-image" />
+          <img src="/public/calculator.JPG" alt="Calculator" className="app-image" />
           <CustomLink to="/calculator">Calculator</CustomLink>
         </li>
         <li>
-          <img src="/public/todolist.jpg" alt="Todo List" className="app-image" />
+          <img src="/public/todolist.JPG" alt="Todo List" className="app-image" />
           <CustomLink to="/TodoForm">Todo List</CustomLink>
         </li>
         <li>
-          <img src="/public/weatherapp.jpg" alt="Weather Forecast" className="app-image" />
+          <img src="/public/weatherapp.JPG" alt="Weather Forecast" className="app-image" />
           <CustomLink to="/WeatherApp">Weather Forecast</CustomLink>
         </li>
       </ul>
