dbbbbdd7230eec2bfaa6d4524d3f1c06a4a17883
Chyisiang <chyisiang8@gmail.com>
2023-06-01 16:40:28 +0800

    very scuffed homepage cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,23 @@
 import { CustomLink } from "./Navbar";
 
 export function Home() {
-    return (
-        <div className="Selection">
-            <h1 className="home-title">Apps</h1>
-            <ul className="homelist">
-                <li>
-                    <CustomLink to="/calculator">Calculator</CustomLink>
-                </li>
-                <li>
-                    <CustomLink to="/TodoForm">Todo List</CustomLink>
-                </li>
-                <li>
-                    <CustomLink to="/WeatherApp">Weather Forecast</CustomLink>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="Selection">
+      <h1 className="home-title">Apps</h1>
+      <ul className="homelist">
+        <li>
+          <img src="/public/calculator.jpg" alt="Calculator" className="app-image" />
+          <CustomLink to="/calculator">Calculator</CustomLink>
+        </li>
+        <li>
+          <img src="/public/todolist.jpg" alt="Todo List" className="app-image" />
+          <CustomLink to="/TodoForm">Todo List</CustomLink>
+        </li>
+        <li>
+          <img src="/public/weatherapp.jpg" alt="Weather Forecast" className="app-image" />
+          <CustomLink to="/WeatherApp">Weather Forecast</CustomLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
